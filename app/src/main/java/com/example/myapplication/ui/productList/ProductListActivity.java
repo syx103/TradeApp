e413ba8d4a4f0cd1384bf6e7916d988ec5eb7245
tengtengwa <1511900943@qq.com>
2020-02-14 12:31:44 +0800

    商品列表和详情页面

---
+++ app/src/main/java/com/example/myapplication/ui/productList/ProductListActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.ui.productList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class ProductListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+
+
+    }
+}
